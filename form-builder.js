305d78dd0be122c547bfa8e4d0e010878aeb99e4
MMnemonic <alberto_mmoura@hotmail.com>
2018-04-09 19:01:33 +0100

    form-builder.js edits

--- form-builder.js
+++ form-builder.js
@@ -20,14 +20,31 @@ function init() {
 			"state", "types", "wholerow", "checkbox" ]
 	});
 
-	var to = false;
-	$('#demo_q').keyup(function () {
-		if(to) { clearTimeout(to); }
-		to = setTimeout(function () {
-			var v = $('#demo_q').val();
-			$('#html1').jstree(true).search(v);
-		}, 250);
-	});
+	$('#using_json').jstree({ 'core' : {
+    'data' : [
+       'Simple root node',
+       {
+         'text' : 'Root node 2',
+         'state' : {
+           'opened' : true,
+           'selected' : true
+         },
+         'children' : [
+           { 'text' : 'Child 1' },
+           'Child 2'
+         ]
+      }
+    ]
+} });
+
+	// var to = false;
+	// $('#demo_q').keyup(function () {
+	// 	if(to) { clearTimeout(to); }
+	// 	to = setTimeout(function () {
+	// 		var v = $('#demo_q').val();
+	// 		$('#html1').jstree(true).search(v);
+	// 	}, 250);
+	// });
 
 
 	$('#html1').jstree({
